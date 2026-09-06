8facabdf92d157cba16af8eb5b85be794b1f7aae
nadazahra675-commits <nadazahra675@gmail.com>
2026-09-06 20:48:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ model = joblib.load('skincare_clean_model.pkl')
 st.title("🧴 Skincare Product Analyzer")
 st.write("Enter product details to predict the economic tier based on ingredients and price.")
 
-price = st.number_input("سعر المنتج ($)", min_value=1.0, max_value=300.0, value=25.0)
-ingredient_count = st.number_input("عدد المكونات في المنتج", min_value=1, max_value=100, value=12)
+price = st.number_input("Product price ($)", min_value=1.0, max_value=300.0, value=25.0)
+ingredient_count = st.number_input("Number of ingredients in the product", min_value=1, max_value=100, value=12)
 
-if st.button("تحليل المنتج"):
+if st.button("Product Analysis"):
     features = np.array([[price, ingredient_count]])
     prediction = model.predict(features)[0]
 
     if prediction == 1:
-        st.success("✅ **النتيجة:** المنتج يعتبر اقتصادي ومناسب للسعر (Affordable).")
+        st.success("✅ **Conclusion: The product is considered economical and reasonably priced. (Affordable).")
     else:
-        st.warning("⚠️ **النتيجة:** المنتج يعتبر مرتفع السعر/فئة فاخرة (Premium).")
+        st.warning("⚠️ :**The product is considered high-priced/luxury-tier. (Premium).")
